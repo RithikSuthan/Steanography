666292981976e95c9ddf791c72e7222133f9b4ec
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-10-21 11:51:05 +0530

    fixed some ui changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { RegisterComponent } from './components/register/register.component';
 import {FormsModule, NgForm} from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
